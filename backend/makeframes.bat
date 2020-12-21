b9f231f9ca897a2e2379c5dec264769b3b4cb3d3
degraffa <ard96@cornell.edu>
2020-12-21 04:37:28 -0500

    actual season 2 fix

--- backend/makeframes.bat
+++ backend/makeframes.bat
@@ -1,10 +1,11 @@
-python .\iro-backend.py s2e01 movies/s2e01.mp4 20 1
-python .\iro-backend.py s2e02 movies/s2e02.mp4 20 1
-python .\iro-backend.py s2e03 movies/s2e03.mp4 20 1
-python .\iro-backend.py s2e04 movies/s2e04.mp4 20 1
-python .\iro-backend.py s2e05 movies/s2e05.mp4 20 1
-python .\iro-backend.py s2e06 movies/s2e06.mp4 20 1
-python .\iro-backend.py s2e07 movies/s2e07.mp4 20 1
-python .\iro-backend.py s2e08 movies/s2e08.mp4 20 1
-python .\iro-backend.py s2e09 movies/s2e09.mp4 20 1
-python .\iro-backend.py s2e10 movies/s2e10.mp4 20 1
+@REM python .\iro-backend.py s2e01 movies/s2e01.mp4 20 1
+@REM python .\iro-backend.py s2e02 movies/s2e02.mp4 20 1
+@REM python .\iro-backend.py s2e03 movies/s2e03.mp4 20 1
+@REM python .\iro-backend.py s2e04 movies/s2e04.mp4 20 1
+@REM python .\iro-backend.py s2e05 movies/s2e05.mp4 20 1
+@REM python .\iro-backend.py s2e06 movies/s2e06.mp4 20 1
+@REM python .\iro-backend.py s2e07 movies/s2e07.mp4 20 1
+@REM python .\iro-backend.py s2e08 movies/s2e08.mp4 20 1
+@REM python .\iro-backend.py s2e09 movies/s2e09.mp4 20 1
+@REM python .\iro-backend.py s2e10 movies/s2e10.mp4 20 1
+python .\iro-backend.py s2e05 movies/s2e05.mkv 20 0.2
